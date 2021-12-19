abda96bdd4ca5e4467eb2dafa8d5325757f55457
ujjwaltyagi2000 <ujjwaltyagi28@gmail.com>
2021-12-19 09:13:10 +0530

    Pluto added

--- hangman.py
+++ hangman.py
@@ -1,4 +1,4 @@
 word_list={
     "Electronics":["Microwave Oven", "Washing Machine", "Television", "Refrigerator", "Toaster",],
-    "Astronomy":["Alpha Centauri", "Orion Nebula", "Saturn", "Venus", "Milky Way",],
+    "Astronomy":["Alpha Centauri", "Orion Nebula", "Saturn", "Venus", "Milky Way","Pluto"],
 }
