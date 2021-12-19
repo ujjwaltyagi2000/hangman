bd580887ff78dacb86cc192dca5eb231408df2b6
ujjwaltyagi2000 <ujjwaltyagi28@gmail.com>
2021-12-19 09:09:04 +0530

    Astronomy field added

--- hangman.py
+++ hangman.py
@@ -1,3 +1,4 @@
 word_list={
-    "Electronics":["Microwave Oven", "Washing Machine", "Television", "Refrigerator", "Toaster", ""]
+    "Electronics":["Microwave Oven", "Washing Machine", "Television", "Refrigerator", "Toaster",],
+    "Astronomy":["Alpha Centauri", "Orion Nebula", "Saturn", "Venus", "Milky Way",],
 }
