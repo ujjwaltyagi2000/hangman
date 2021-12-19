f5e020743ad830d4fee87645a6a66e60ac491d25
ujjwaltyagi2000 <ujjwaltyagi28@gmail.com>
2021-12-19 09:15:14 +0530

    Testing

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 word_list={
     "Electronics":["Microwave Oven", "Washing Machine", "Television", "Refrigerator", "Toaster",],
     "Astronomy":["Alpha Centauri", "Orion Nebula", "Saturn", "Venus", "Milky Way","Pluto"],
+    "Astronomy":["Alpha Centauri", "Orion Nebula", "Saturn", "Venus", "Milky Way","Pluto"],
 }
